1c4e698dca8fba0d968fac766328cb352780bd3b
underscore95 <107675637+underscore95@users.noreply.github.com>
2026-01-19 15:14:45 +0000

    simple example

--- src/main/java/dev/hytalemodding/ExamplePlugin.java
+++ src/main/java/dev/hytalemodding/ExamplePlugin.java
@@ -1,10 +1,8 @@
 package dev.hytalemodding;
 
-import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-import dev.hytalemodding.commands.*;
-import dev.hytalemodding.events.OpenGuiListener;
+import dev.hytalemodding.commands.ShowPageCommand;
 
 import javax.annotation.Nonnull;
 
@@ -16,12 +14,6 @@ public class ExamplePlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
-        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, OpenGuiListener::openGui);
-
-        this.getCommandRegistry().registerCommand(new ShowHudCommand("showhud", "Example command"));
-        this.getCommandRegistry().registerCommand(new HideHudCommand("hidehud", "Example command"));
-        this.getCommandRegistry().registerCommand(new UpdateHudCommand("updatehud", "Example command"));
         this.getCommandRegistry().registerCommand(new ShowPageCommand("showpage", "Example command"));
-        this.getCommandRegistry().registerCommand(new HidePageCommand("hidepage", "Example command"));
     }
 }
